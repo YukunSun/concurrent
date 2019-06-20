427999de8562ba9615b670063541b70ea0f1ac21
sunyk <sunyukun@shopin.cn>
2019-06-20 17:02:28 +0800

    algorithm,use two stack implement a queue

---
+++ src/main/java/algorithm/ImplementQueueWithStack.java
@@ -0,0 +1,37 @@
+package algorithm;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * @author: sun.yukun@foxmail.com
+ * Time: 2019-06-20 16:49
+ * Blog: coderdaily.net
+ * <p>
+ * 用两个栈实现一个队列
+ */
+public class ImplementQueueWithStack {
+    public static void main(String[] args) {
+        Stack<Integer> s1 = new Stack<Integer>();
+        Stack<Integer> s2 = new Stack<Integer>();
+        Queue<Integer> queue = new PriorityQueue<Integer>();
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+        queue.add(5);
+
+        //s1，用作入栈
+        while (!queue.isEmpty()) {
+            s1.push(queue.poll());
+        }
+
+        //s2 用作出栈
+        while (!s1.isEmpty()) {
+            s2.push(s1.pop());
+        }
+
+        System.out.println(s2);
+    }
+}
