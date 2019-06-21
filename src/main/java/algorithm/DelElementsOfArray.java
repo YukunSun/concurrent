f2a329420a7ff31c75976f1e2d0b4221ba83091c
sunyk <sunyukun@shopin.cn>
2019-06-21 10:10:32 +0800

    refactor

--- src/main/java/algorithm/DelElementsOfArray.java
+++ src/main/java/algorithm/DelElementsOfArray.java
@@ -29,6 +29,7 @@ public class DelElementsOfArray {
 
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(3);
         list.add(1);
         list.add(3);
         list.add(2);
