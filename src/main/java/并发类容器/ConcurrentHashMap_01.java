9f2a523b64b86fcb73286c797c4ee13c8863d972
sunyk <sunyukun@shopin.cn>
2019-07-02 12:22:31 +0800

    refactor

--- src/main/java/并发类容器/ConcurrentHashMap_01.java
+++ src/main/java/并发类容器/ConcurrentHashMap_01.java
@@ -11,27 +11,30 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ConcurrentHashMap_01 {
     private static ConcurrentHashMap<Integer, Integer> map = new ConcurrentHashMap<Integer, Integer>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         new Thread("Thread1") {
             @Override
             public void run() {
                 map.put(3, 33);
             }
-        };
+        }.start();
 
         new Thread("Thread2") {
             @Override
             public void run() {
                 map.put(4, 44);
             }
-        };
+        }.start();
 
-        new Thread("Thread3") {
+        Thread t3 = new Thread("Thread3") {
             @Override
             public void run() {
                 map.put(7, 77);
             }
         };
+        t3.start();
+        t3.join();
+
         System.out.println(map);
     }
 }
