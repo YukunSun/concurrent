1ccd85bf30c0644fca032047d79a4daded4c9c88
sunyk <sunyukun@shopin.cn>
2020-02-19 08:58:49 +0800

    comment

--- src/test/java/algorithm/sort/BubbleTest.java
+++ src/test/java/algorithm/sort/BubbleTest.java
@@ -3,15 +3,18 @@ package algorithm.sort;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 /**
  * @author: sun.yukun@foxmail.com
  * Time: 2020/2/18 22:55
  * Blog: coderdaily.net
  */
 public class BubbleTest {
+    /**
+     * 核心逻辑：
+     * <p>
+     * 1 两层循环：一个指针控制排好序的部分，另一个控制待排的部分（待排部分的两个指针可以用其中之一推断出来）
+     * 2 排序时需要交换元素
+     */
     @Test
     public void bubbleTest() {
         int[] expect = {1, 3, 5, 7};
