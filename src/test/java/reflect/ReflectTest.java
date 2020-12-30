2f68e16ae371464714ea05526fa61ba176b1c599
sunyk <sunyukun@flipboard.cn>
2020-12-30 15:50:25 +0800

    class

--- src/test/java/reflect/ReflectTest.java
+++ src/test/java/reflect/ReflectTest.java
@@ -14,43 +14,53 @@ import java.lang.reflect.Modifier;
  **/
 public class ReflectTest {
     @Test
-    public void reflectDemo() throws ClassNotFoundException {
-        //get methods
+    public void reflectDemo() throws ClassNotFoundException, NoSuchMethodException {
+//        1 get methods
         Method[] methods = ReflectTest.class.getDeclaredMethods();
         for (Method method : methods) {
             System.out.println(method);
         }
 
-//       get Class
+//       2 get Class
         Class c = ReflectTest.class;
         Assert.assertEquals(true, c instanceof Class);
 
         Class c2 = Class.forName("reflect.ReflectTest");
         Assert.assertEquals(c, c2);
-//        class name
+//        3 class name
         Assert.assertEquals("reflect.ReflectTest", c.getName());
         Assert.assertEquals("ReflectTest", c.getSimpleName());
-//        modifier
+//        4 modifier
         Assert.assertEquals(true, Modifier.isPublic(c.getModifiers()));
         Assert.assertEquals(false, Modifier.isAbstract(c.getModifiers()));
-//        package
+//        5 package
         Assert.assertEquals("reflect", c.getPackageName());
-//        super class
+//        6 super class
         Assert.assertEquals(Object.class, c.getSuperclass());
-//        implemented interfaces
+//        7 implemented interfaces
         Class c3 = Thread.class;
         for (Class itf : c3.getInterfaces()) {
             System.out.println(itf);
         }
-//        constructor
+//        8 constructor
         for (Constructor constructor : c3.getConstructors()) {
             System.out.println(constructor);
         }
-//        fields
+        Constructor constructor = c3.getConstructor(new Class[]{Runnable.class, String.class});
+        Assert.assertEquals("public java.lang.Thread(java.lang.Runnable,java.lang.String)", constructor.toString());
+//        利用Constructor对象实例化一个类
+        System.out.println(constructor.getParameterTypes());
+        try {
+            Thread thread = (Thread) constructor.newInstance(new Thread(), "11");
+            System.out.println(thread);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+//        9 fields
         for (Field f : c3.getFields()) {
             System.out.println(f);
         }
-//        annotations
+//        10 annotations
         for (Annotation annotation : Runnable.class.getAnnotations()) {
             System.out.println(annotation);
         }
