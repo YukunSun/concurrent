fa82d729459f3fa5536c2eb7494de34e16b7922e
sunyk <sunyukun@shopin.cn>
2021-03-08 15:04:37 +0800

    synchronized is re entrant

--- src/test/java/i2021/ReEntrantLockDemo.java
+++ src/test/java/i2021/ReEntrantLockDemo.java
@@ -1,5 +1,8 @@
 package i2021;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @author: sun.yukun@foxmail.com
  * Time: 2021/3/8 11:48
@@ -8,6 +11,7 @@ package i2021;
 public class ReEntrantLockDemo {
     static Object lock = new Object();
 
+    //eg1：被synchronized的lock对象是可重入的
     static void func() {//这就说明 synchronized 是可重入的
         synchronized (lock) {
             System.out.println("layer 1");
@@ -20,6 +24,7 @@ public class ReEntrantLockDemo {
         }
     }
 
+    //eg2：被synchronized修饰的不同的方法也是可重入的
     static synchronized void f1() {
         System.out.println("layer 1");
         f2();
@@ -34,8 +39,23 @@ public class ReEntrantLockDemo {
         System.out.println("layer 3");
     }
 
+    //eg3:也是可重入的
+    static Lock lock2 = new ReentrantLock();
+
+    static void func5() {
+        lock2.lock();
+        System.out.println("reentrant 1");
+
+        lock2.lock();
+        System.out.println("reentrant 2");
+        lock2.unlock();
+
+        lock2.unlock();
+    }
+
     public static void main(String[] args) {
 //        func();
-        f1();
+//        f1();
+        func5();//lock,unlock需要成对出现，否则t2可能获取不了锁.
     }
 }
