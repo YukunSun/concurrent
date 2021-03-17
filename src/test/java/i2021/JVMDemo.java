dac4f046cd11903622af6a0cc9b9ac69478f9687
sunyk <sunyukun@shopin.cn>
2021-03-17 17:37:26 +0800

    phantom,referenceQueue

--- src/test/java/i2021/JVMDemo.java
+++ src/test/java/i2021/JVMDemo.java
@@ -2,8 +2,12 @@ package i2021;
 
 import org.junit.Test;
 
+import java.lang.ref.PhantomReference;
+import java.lang.ref.ReferenceQueue;
 import java.lang.ref.SoftReference;
 import java.lang.ref.WeakReference;
+import java.util.HashMap;
+import java.util.WeakHashMap;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -89,13 +93,72 @@ public class JVMDemo {
     }
 
     @Test
-    public void weakHashMapTest() {
+    public void weakHashMapTest() throws InterruptedException {
+        //1 强引用的 HashMap
+        HashMap<Integer, String> hashMap = new HashMap<>();
+        Integer key = 1;
+        hashMap.put(key, "v1");
 
+        System.out.println("hashMap = " + hashMap);
+
+        key = null;
+        System.gc();
+        TimeUnit.SECONDS.sleep(3);
+
+        System.out.println("hashMap.get(1) = " + hashMap.get(1));//hashMap.get(1) = v1
+
+        //2 弱引用的WeakHashMap
+        WeakHashMap<Integer, String> weakHashMap = new WeakHashMap();
+        Integer k2 = 2;
+        weakHashMap.put(k2, "v2");
+        System.out.println("weakHashMap = " + weakHashMap);
+
+        k2 = null;
+        System.gc();//也就是发生gc之后，v会被回收
+        TimeUnit.SECONDS.sleep(3);
+
+        System.out.println("weakHashMap.get(1) = " + weakHashMap.get(1));//weakHashMap.get(1) = null
     }
 
-
+    /**
+     * soft,weak,phantom都会把回收的对象放到ReferenceQueue中
+     */
     @Test
-    public void phantomReferenceTest() {
+    public void referenceQueueTest() throws InterruptedException {
+        Object obj = new Object();
+        ReferenceQueue<Object> refQueue = new ReferenceQueue<>();
+        WeakReference<Object> softRef = new WeakReference<>(obj, refQueue);
+        System.out.println("obj = " + obj);
+        System.out.println("softRef.get() = " + softRef.get());//1 softRef.get() = java.lang.Object@2b552920
+        System.out.println("refQueue.poll() = " + refQueue.poll());//2 refQueue.poll() = null
 
+        obj = null;
+        System.gc();
+        TimeUnit.SECONDS.sleep(3);
+
+        System.out.println("obj = " + obj);
+        System.out.println("softRef.get() = " + softRef.get());//1.1 softRef.get() = null
+        System.out.println("refQueue.poll() = " + refQueue.poll());//2.1 refQueue.poll() = java.lang.ref.WeakReference@42f93a98
+    }
+
+    /**
+     * 代码同referenceQueueTest，
+     */
+    @Test
+    public void phantomReferenceTest() throws InterruptedException {
+        Object obj = new Object();
+        ReferenceQueue<Object> refQueue = new ReferenceQueue<>();
+        PhantomReference<Object> softRef = new PhantomReference<>(obj, refQueue);
+        System.out.println("obj = " + obj);
+        System.out.println("softRef.get() = " + softRef.get());//null，却get不到！！！所以如果没有这个queue，虚引用竟然没法工作了
+        System.out.println("refQueue.poll() = " + refQueue.poll());//null
+
+        obj = null;
+        System.gc();
+        TimeUnit.SECONDS.sleep(3);
+
+        System.out.println("obj = " + obj);
+        System.out.println("softRef.get() = " + softRef.get());//null
+        System.out.println("refQueue.poll() = " + refQueue.poll());//refQueue.poll() = java.lang.ref.PhantomReference@726f3b58
     }
 }
