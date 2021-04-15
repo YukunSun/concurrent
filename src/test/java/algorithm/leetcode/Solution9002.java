e5944993d8c07423ee35f7a8c4ad2d0326a4022f
sunyk <sunyukun@shopin.cn>
2021-04-16 00:51:52 +0800

    day 9:log

--- src/test/java/algorithm/leetcode/Solution9002.java
+++ src/test/java/algorithm/leetcode/Solution9002.java
@@ -5,14 +5,13 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author: sun.yukun@foxmail.com
  * Time: 2021/4/15 12:04
  * Blog: bengle.me
  * <p>
- * 打印二叉树的每层的最左/最右节点
+ * 打印二叉树的每层的最左/最右节点，即左视图/右视图
  */
 public class Solution9002 {
     static TreeNode nodeDemo = new TreeNode(3, new TreeNode(9), new TreeNode(20));
@@ -32,13 +31,6 @@ public class Solution9002 {
         printRight(nodeDemo);
     }
 
-    public List<Integer> left(TreeNode root) {
-
-
-        return null;
-    }
-
-
     public void printRight(TreeNode root) {
         if (root == null) {
             return;
@@ -50,8 +42,8 @@ public class Solution9002 {
             TreeNode lastNode = queue.get(levelNum - 1);//每层的最后一个节点，即右视图
 //            TreeNode lastNode = queue.get(0);//每层的第一个节点，即左视图
             for (int i = 0; i < levelNum; i++) {
-                TreeNode node = queue.remove(0);
-                if (node == lastNode) {
+                TreeNode node = queue.remove(0);//会一个个的往后移动，相当于一个指针
+                if (node == lastNode) {//暂存的lastNode与当前节点进行比较，因为可能无法跟后边的节点区分
                     System.out.println(node.val);
                 }
                 if (node.left != null) {
@@ -62,5 +54,13 @@ public class Solution9002 {
                 }
             }
         }
+    }
+
+    /**
+     * 遍历同时存储当前深度与max深度，判断当前是否最深节点，若是，则加入当前节点到result队列中，先递归右子树，再递归左子树；否则，依次递归右子树和左子树
+     */
+    @Test
+    public void rightView2() {
+
     }
 }
