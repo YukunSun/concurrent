26401c3fd3d4e51e4d054a9cde5b2e517612ead4
sunyk <sunyukun@shopin.cn>
2021-05-05 17:13:18 +0800

    day 28:yanghui triangle,v2

--- src/test/java/algorithm/leetcode/Solution0118.java
+++ src/test/java/algorithm/leetcode/Solution0118.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -64,5 +65,37 @@ public class Solution0118 {
         Assert.assertThrows(IllegalArgumentException.class, () -> helper(1, 2));
         Assert.assertEquals(helper(2, 1), 1);
         Assert.assertEquals(helper(4, 2), 3);
+    }
+
+    @Test
+    public void generate2Test() {
+        List<List<Integer>> list = generate2(5);
+        System.out.println(list);
+    }
+
+    /**
+     * v2:直接复用上一层计算过的结果，而不是给定参数后再重新计算每个值；相对于v1来讲，基线条件是以行为单位的，比以某个元素为单位省了太多计算量了
+     *
+     * @param numRows
+     * @return
+     */
+    public List<List<Integer>> generate2(int numRows) {
+        if (numRows < 1) {
+            return result;
+        }
+        if (numRows == 1) {
+            result.add(Arrays.asList(1));
+            return result;
+        }
+        result = generate2(numRows - 1);//我在这里用的是i--，有问题
+        List<Integer> list = new ArrayList<>();
+        list.add(1);//添加第一个元素
+        for (int j = 1; j < numRows - 1; j++) {
+            List<Integer> aboveList = result.get(numRows - 2);
+            list.add(aboveList.get(j - 1) + aboveList.get(j));
+        }
+        list.add(1);//添加最后一个元素
+        result.add(list);
+        return result;
     }
 }
